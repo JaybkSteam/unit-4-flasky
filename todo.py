6f9a1894eb4d675dedff2b0a673bcb6138f12b0d
Jaybksteam <jedouard@brooklynsteamcenter.org>
2024-01-04 10:55:03 -0500

    changes

--- todo.py
+++ todo.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template, request
+import pymysql
+import pymysql.cursors
+from pprint import pprint as print
+xw
 
 
 todolist = ["Go to sleep when im home", "Gaming", "Read books/comics"]
